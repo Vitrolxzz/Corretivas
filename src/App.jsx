aa1aeac125c169899a468cadaebe245c0d5687c5
Vitrolxzz <vittorlacksmy@gmail.com>
2026-09-18 16:01:42 -0300

    10.6s → 10.55s

--- src/App.jsx
+++ src/App.jsx
@@ -542,7 +542,7 @@ function DisplayModeView({ activeView, dashboard, appointments, turnstiles, sele
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setRecurrencePeriod((prev) => (prev === '1m' ? '6m' : '1m'));
-		}, 10600);
+		}, 10550);
 
 		return () => clearInterval(interval);
 	}, []);
